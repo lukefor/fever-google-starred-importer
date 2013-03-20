fe49edb1f8ecb9a1414b435bea4312de010871fc
Luke Foreman <admin@imgkk.com>
2013-03-20 16:08:22 +0000

    removed php 5.4 only syntax

--- import.php
+++ import.php
@@ -8,7 +8,7 @@ $items = $json['items'];
 foreach($items as $item){
 	$feedUrl = substr($item['origin']['streamId'], 5);
 	$stmt = $db->prepare("select id from fever_feeds where url=?");
-	$stmt->execute([$feedUrl]);
+	$stmt->execute(array($feedUrl));
 	$feedId = (int)$stmt->fetchColumn(0);
 	if($feedId > 0){
 		$stmt = $db->prepare("insert into fever_items set feed_id = ?, uid = ?, title = ?, author = ?, description = ?, link = ?, url_checksum = ?, read_on_time = 0, is_saved = 1, created_on_time = ?, added_on_time = ?");
@@ -18,8 +18,8 @@ foreach($items as $item){
 		else
 			$content = $item['content']['content'];
 			
-		$stmt->execute([
+		$stmt->execute(array(
 			$feedId, $item['id'], $item['title'], $item['author'], $content, $item['alternate'][0]['href'], sprintf('%u', crc32($item['alternate'][0]['href'])), $item['published'], $item['published'],
-		]);		
+		));		
 	}
 }
